cdc0fb8cc9b8549c9a78d48e4195f37d20aa145b
AhmedElFaleh <ahmed.elfleh.1@gmail.com>
2019-02-07 10:50:38 +0200

    Uploaded all needed files

---
+++ cronimplementaitonscript.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /etc/cron.d/
+sudo touch backup
+echo "00 01 1 * * root rsnapshot monthly || /orkitools/report
+30 01 */7 * * root rsnapshot weekly || /orkitools/report
+00 02 * * * root rsnapshot daily || /orkitools/report" | sudo tee --append backup
