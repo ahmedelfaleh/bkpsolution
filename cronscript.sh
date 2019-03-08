eb94fcc008c6391131b7d772cf4dd065c5177edb
Ahmed El-Faleh <ahmed.elfleh.1@gmail.com>
2019-03-08 17:17:35 +0000

    Update cronscript.sh

--- cronscript.sh
+++ cronscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /etc/cron.d/
+sudo rm -f /etc/cron.d/backup
 sudo touch backup
-> /etc/cron.d/backup
 echo "00 01 1 * * root rsnapshot monthly || /orkitools/report
 30 01 */7 * * root rsnapshot weekly || /orkitools/report
 00 02 * * * root rsnapshot daily || /orkitools/report" | sudo tee --append backup
