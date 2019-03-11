4f23196d4bbbac0457d72da4db1e295c843c01d2
Ahmed El-Faleh <ahmed.elfleh.1@gmail.com>
2019-03-11 14:41:25 +0000

    Removing sudo and touch lines

--- cronscript.sh
+++ cronscript.sh
@@ -2,13 +2,12 @@
 cd /etc/cron.d/
 if [ ! -f backup ]
 then
-sudo touch backup
 echo "00 01 1 * * root rsnapshot monthly || /orkitools/report
 30 01 */7 * * root rsnapshot weekly || /orkitools/report
-00 02 * * * root rsnapshot daily || /orkitools/report" | sudo tee --append backup
+00 02 * * * root rsnapshot daily || /orkitools/report" | tee --append backup
 else
-sudo rm -f /etc/cron.d/backup
+rm -f /etc/cron.d/backup
 echo "00 01 1 * * root rsnapshot monthly || /orkitools/report
 30 01 */7 * * root rsnapshot weekly || /orkitools/report
-00 02 * * * root rsnapshot daily || /orkitools/report" | sudo tee --append backup
+00 02 * * * root rsnapshot daily || /orkitools/report" | tee --append backup
 fi
